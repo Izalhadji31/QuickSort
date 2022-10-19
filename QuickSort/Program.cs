7bd9cde99195a0092851617d1de36fb9133fe3db
Izalhadji31 <115075859+Izalhadji31@users.noreply.github.com>
2022-10-19 17:13:17 +0700

    int low, int high

--- QuickSort/Program.cs
+++ QuickSort/Program.cs
@@ -35,6 +35,21 @@ namespace QuickSort
 
             //get array elements
             for(int i =0;i < n: i++)
+            {
+                Console.Write("<" + (i + 1) + ">");
+                string s1 = Console.ReadLine();
+                arr[i] = Int32.Parse(s1);
+            }
         }
+        //swaps the element at index x with the element at index y
+        void swap (int x, int y)
+        {
+            int temp;
+
+            temp = arr[x];
+            arr[x] = arr[y];
+            arr[y] = temp;
+        }
+        public void q_sort(int low,int high)
     }
 }
