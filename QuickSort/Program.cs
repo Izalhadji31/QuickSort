b2ab2b2eb59d758ba0a8ee9d33f5ea432d0bc4bc
Izalhadji31 <115075859+Izalhadji31@users.noreply.github.com>
2022-10-19 16:50:47 +0700

    void input

--- QuickSort/Program.cs
+++ QuickSort/Program.cs
@@ -9,5 +9,14 @@ namespace QuickSort
     class Program
     {
         //array of integers to hold values
+        private int[] arr = new int[20];
+        private int cmp_count = 0; // number of comparasion
+        private int mov_count = 0; // number of data movements
+
+        //number of elements in array
+        private int n;
+
+
+        void input()
     }
 }
